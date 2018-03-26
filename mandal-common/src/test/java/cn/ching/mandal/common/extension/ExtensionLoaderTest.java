6fca56b5f6193dc0d79681a9fff012f999e159c0
chi.zhang <chi.zhang09@ele.me>
2018-03-26 21:47:34 +0800

    test compiler

--- mandal-common/src/test/java/cn/ching/mandal/common/extension/ExtensionLoaderTest.java
+++ mandal-common/src/test/java/cn/ching/mandal/common/extension/ExtensionLoaderTest.java
@@ -1,25 +1,42 @@
 package cn.ching.mandal.common.extension;
 
 
-import cn.ching.mandal.common.Constants;
+import cn.ching.mandal.common.URL;
 import cn.ching.mandal.common.compiler.Compiler;
-import cn.ching.mandal.common.extension.filter.Filter;
+import cn.ching.mandal.common.threadpool.ThreadPool;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.util.Enumeration;
+import java.lang.reflect.Method;
 
 public class ExtensionLoaderTest{
 
 
     @Test
-    public void getExtensionLoader() throws Exception {
+    public void TestCompiler() throws Exception {
         try {
             Compiler compiler = ExtensionLoader.getExtensionLoader(Compiler.class).getExtension("jdk");
+
+            StringBuilder sb = new StringBuilder(ExtensionLoaderTest.class.getPackage() + ";");
+            sb.append("\n import " + ExtensionLoader.class.getName() + ";");
+            sb.append("\n public class Test$Adaptive {");
+            sb.append("\n public String outprint(int i){");
+            sb.append("\n return(\"haha\" + i);");
+            sb.append("\n }");
+            sb.append("\n }");
+            Class<?> cls = compiler.compiler(sb.toString(), getClass().getClassLoader());
+            System.out.println(cls.getName());
+            Method[] methods = cls.getMethods();
+            for (Method method : methods) {
+                try {
+                    if (method.getName() == "outprint"){
+                        String res = (String) method.invoke(cls.newInstance(), 123);
+                        Assert.assertEquals("haha123", res);
+                    }
+                }catch (Exception e){
+                    System.err.println(e.getMessage());
+                }
+            }
         }catch (IllegalStateException e){
             System.err.println(e);
         }
